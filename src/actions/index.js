3d4786e4cca1d08b959b405c4228675e4d0cce17
Pawel Karauda <=>
2018-09-19 23:31:05 +0200

    Done Son

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 export const FETCH_POSTS = "fetch_posts";
 export const ADD_POST = "add_post";
+export const FETCH_POST = "fetch_post";
+export const DELETE_POST = "delete_post";
 
 const baseURL = "http://reduxblog.herokuapp.com";
 const API_KEY = "?key=rocketscience";
@@ -25,5 +27,25 @@ export function addPost(values, callback) {
   return {
     type: ADD_POST,
     payload: request
+  };
+}
+
+export function fetchPost(id) {
+  const request = axios.get(`${baseURL}/api/posts/${id}${API_KEY}`);
+
+  return {
+    type: FETCH_POST,
+    payload: request
+  };
+}
+
+export function deletePost(id, callback) {
+  const request = axios
+    .delete(`${baseURL}/api/posts/${id}${API_KEY}`)
+    .then(() => callback());
+
+  return {
+    type: DELETE_POST,
+    payload: id
   };
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,55 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchPost, deletePost } from "../actions";
+import { Link } from "react-router-dom";
 
 class Post extends Component {
+  componentDidMount() {
+    console.log(this.props.match.params.id);
+    const id = this.props.match.params.id;
+    this.props.fetchPost(id);
+  }
+
+  onDeleteClick() {
+    const id = this.props.match.params.id;
+    this.props.deletePost(id, () => {
+      this.props.history.push("/");
+    });
+  }
+
   render() {
-    return <div>Post show!</div>;
+    const { post } = this.props;
+
+    if (!post) {
+      return <div>Loading..</div>;
+    }
+
+    return (
+      <div>
+        <Link to="/"> Go back </Link>
+        <button
+          onClick={this.onDeleteClick.bind(this)}
+          className="btn-btn-danger pull-xs-right"
+        >
+          Delete post
+        </button>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categiores}</h6>
+        <p>{post.content}</p>
+        <Link to="/"> Delete </Link>
+      </div>
+    );
   }
 }
 
-export default Post;
+function mapStateToProps({ posts }, ownProps) {
+  return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(
+  mapStateToProps,
+  {
+    fetchPost,
+    deletePost
+  }
+)(Post);
